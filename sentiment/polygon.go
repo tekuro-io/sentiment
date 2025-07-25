15dabf0e6e0c1150cb408c18901d6cebd988ca61
alexmazur <alexmazur6@gmail.com>
2025-07-25 01:50:55 -0600

    news from 72 hours from today

--- sentiment/polygon.go
+++ sentiment/polygon.go
@@ -52,7 +52,7 @@ func (p *Polygon) News(ctx context.Context, ticker string) *iter.Iter[models.Tic
 	sort := models.Sort("published_utc")
 	order := models.Order("asc")
 	limit := 1
-	twelveHoursAgo := models.Millis(time.Now().Add(-12 * time.Hour))
+	twelveHoursAgo := models.Millis(time.Now().Add(-72 * time.Hour))
 
 	params := models.ListTickerNewsParams{
 		TickerEQ:        &ticker,
